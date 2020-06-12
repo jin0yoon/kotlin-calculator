cd9c141fcc01ccf6952582459ab076d803c36e23
Yoon Jinyeong <wwwz4805@naver.com>
2020-06-12 22:14:41 +0900

    Create new project

---
+++ app/src/main/java/co/kr/jinyeong/kotlin_calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.kr.jinyeong.kotlin_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
